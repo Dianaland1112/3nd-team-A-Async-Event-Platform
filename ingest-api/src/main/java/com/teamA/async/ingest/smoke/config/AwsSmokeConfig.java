b9ad720304ab81a8d7988120b255c64ffdb9d734
Wish <asgvcx@naver.com>
2026-01-03 01:11:24 +0900

    fix/ 자격증명 권한 해결
    related to #4

--- ingest-api/src/main/java/com/teamA/async/ingest/smoke/config/AwsSmokeConfig.java
+++ ingest-api/src/main/java/com/teamA/async/ingest/smoke/config/AwsSmokeConfig.java
@@ -3,6 +3,7 @@ package com.teamA.async.ingest.smoke.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -11,16 +12,24 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 public class AwsSmokeConfig {
 
     @Bean
-    DynamoDbClient dynamoDbClient(@Value("${aws.region}") String region) {
+    DynamoDbClient dynamoDbClient(
+            @Value("${aws.region}") String region,
+            @Value("${aws.profile:wish}") String profile //여기 iam 롤에 맞춰 변경!
+    ) {
         return DynamoDbClient.builder()
                 .region(Region.of(region))
+                .credentialsProvider(ProfileCredentialsProvider.create(profile))
                 .build();
     }
 
     @Bean
-    SqsClient sqsClient(@Value("${aws.region}") String region) {
+    SqsClient sqsClient(
+            @Value("${aws.region}") String region,
+            @Value("${aws.profile:wish}") String profile//여기 iam 롤에 맞춰 변경!
+    ) {
         return SqsClient.builder()
                 .region(Region.of(region))
+                .credentialsProvider(ProfileCredentialsProvider.create(profile))
                 .build();
     }
 }
